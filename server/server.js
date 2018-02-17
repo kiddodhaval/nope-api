dcba5a5ad2dbfa45a35583a6fce7aa55be35bd25
chhedadhaval120386 <=>
2018-02-17 19:34:23 +0530

    Added get routes for all and particular todo and tests for the same

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const {ObjectID} = require('mongodb')
 
 const mongoose = require('../db/mongoose')
 const {Todo} = require('../models/todo')
@@ -16,9 +17,31 @@ app.post('/todos', (req, res) => {
     newTodo.save().then((result) => {
        res.send(result)
     }).catch((err) => {
-       res.status(500).send(err)
+       res.status(400).send(err)
     })
 })
+
+app.get('/todos', (req, res) => {
+   Todo.find().then((todos) => {
+      res.send(todos)
+   }).catch((err) => {
+      res.status(400).send(err)
+   })
+})
+
+app.get('/todos/:id', (req, res) => {
+   const id = req.params.id
+    if(!ObjectID.isValid(id)) return res.status(404).send('Invalid ID')
+
+    Todo.findById(id).then((todo) => {
+        if(!todo) return res.status(404).send('No Todo found')
+
+       res.send(todo)
+    }).catch((err) => {
+       res.status(404).send(err)
+    })
+})
+
 app.listen(3000, () => {
    console.log('Server is up on port 3000')
 })
--- test/server.test.js
+++ test/server.test.js
@@ -4,6 +4,20 @@ const {Todo} = require('../models/todo')
 
 const {app} = require('../server/server')
 
+const todos = [
+    {
+        text: 'First test todo'
+    }, {
+        text: 'Second test todo'
+    }
+];
+
+beforeEach((done) => {
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos)
+    }).then(() => done())
+})
+
 describe('POST /todos', () => {
     it('should add a new todo', (done) => {
         const text = 'Testing todos post'
@@ -18,7 +32,7 @@ describe('POST /todos', () => {
                 if (err) return done(err)
 
                 Todo.find().then((todos) => {
-                    // expect(todos.length).toBe(1)
+                    expect(todos.length).toBe(3)
                     expect(todos[todos.length - 1].text).toBe(text)
                     done()
                 }).catch((e) => done(e))
@@ -34,7 +48,19 @@ describe('POST /todos', () => {
         request(app)
             .post('/todos')
             .send({})
-            .expect(500)
+            .expect(400)
+            .end(done)
+    });
+})
+
+describe('GET /todos', () => {
+    it('should return all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.length).toBe(2)
+            })
             .end(done)
     });
 })
