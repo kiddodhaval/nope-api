d65c73749e4760237a9946c7c4e6dabd3b605b69
chhedadhaval120386 <=>
2018-02-17 19:57:26 +0530

    Created Delete route for single todo

--- server/server.js
+++ server/server.js
@@ -15,35 +15,46 @@ app.post('/todos', (req, res) => {
         text: req.body.text
     })
     newTodo.save().then((result) => {
-       res.send(result)
+        res.send(result)
     }).catch((err) => {
-       res.status(400).send(err)
+        res.status(400).send(err)
     })
 })
 
 app.get('/todos', (req, res) => {
-   Todo.find().then((todos) => {
-      res.send(todos)
-   }).catch((err) => {
-      res.status(400).send(err)
-   })
-})
-
-app.get('/todos/:id', (req, res) => {
-   const id = req.params.id
-    if(!ObjectID.isValid(id)) return res.status(404).send('Invalid ID')
-
-    Todo.findById(id).then((todo) => {
-        if(!todo) return res.status(404).send('No Todo found')
-
-       res.send(todo)
+    Todo.find().then((todos) => {
+        res.send(todos)
     }).catch((err) => {
-       res.status(404).send(err)
+        res.status(400).send(err)
     })
 })
 
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id
+    if (!ObjectID.isValid(id)) return res.status(404).send('Invalid ID')
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) return res.status(404).send('No Todo found')
+
+        res.send(todo)
+    }).catch((err) => res.status(404).send(err))
+})
+
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id
+    if (!ObjectID.isValid(id)) return res.status(404).send('Invalid ID')
+
+    Todo.remove({
+        _id: id
+    }).then((todo) => {
+        if (!todo) return res.status(404).send('No Todo found')
+
+        res.send(todo)
+    }).catch((err) => res.status(404).send(err))
+})
+
 app.listen(3000, () => {
-   console.log('Server is up on port 3000')
+    console.log('Server is up on port 3000')
 })
 
 module.exports = {app}
